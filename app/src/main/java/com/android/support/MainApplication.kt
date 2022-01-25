d05d09d809ab4fcf1ab7700b50280bf8d0bbd4ba
quangpv <quangpv.uit@gmail.com>
2022-01-25 15:43:41 +0700

    Publish with javadoc (Fix exclude module overlap)

--- app/src/main/java/com/android/support/MainApplication.kt
+++ app/src/main/java/com/android/support/MainApplication.kt
@@ -1,14 +1,14 @@
 package com.android.support
 
-import android.app.Application
 import android.support.core.savedstate.SavedStateHandlerFactory
 import android.support.di.LifecycleLookup
 import android.support.di.ShareScope
 import android.support.di.dependencies
+import androidx.multidex.MultiDexApplication
 import com.android.support.app.appModule
 
 @Suppress("unused")
-class MainApplication : Application() {
+class MainApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
