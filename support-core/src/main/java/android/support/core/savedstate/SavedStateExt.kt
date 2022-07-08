793f72b4740e4db76c7d627280dde030b286c150
quangpv <quangpv.uit@gmail.com>
2022-07-08 23:10:00 +0700

    Update saved state ext

--- support-core/src/main/java/android/support/core/savedstate/SavedStateExt.kt
+++ support-core/src/main/java/android/support/core/savedstate/SavedStateExt.kt
@@ -8,7 +8,7 @@ import androidx.savedstate.SavedStateRegistry
 internal val SavedStateHandle.saveStateProvider get() = savedStateProvider()
 
 internal fun createSaveStateHandle(
-    registry: SavedStateRegistry, lifecycle: Lifecycle, key: String
+    registry: SavedStateRegistry, lifecycle: Lifecycle, key: String,
 ): SavedStateHandle {
-    return SavedStateHandleController.create(registry, lifecycle, key, Bundle()).handle
+    return LegacySavedStateHandleController.create(registry, lifecycle, key, Bundle()).handle
 }
